e2c9b6a433dd0b12b2f666aa4752307c788a346d
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-15 20:12:04 +0200

    fixed render

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,12 +3,14 @@ import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import s from './ImageGallery.module.css';
 
 const ImageGallery = ({ searchQuery, onClick }) => {
-  <ul className={s.ImageGallery}>
-    {searchQuery &&
-      searchQuery.map((img, i) => (
-        <ImageGalleryItem key={img.id} img={img} onClick={() => onClick(i)} />
-      ))}
-  </ul>;
+  return (
+    <ul className={s.ImageGallery}>
+      {searchQuery &&
+        searchQuery.map((img, i) => (
+          <ImageGalleryItem key={img.id} img={img} onClick={() => onClick(i)} />
+        ))}
+    </ul>
+  );
 };
 
 export default ImageGallery;
