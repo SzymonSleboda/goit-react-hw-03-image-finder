42246064cacc26506f4bdbb8c7498f49ad8edbe1
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-15 14:56:03 +0200

    added App.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { toast } from 'react-toastify';
+import s from './Searchbar.module.css';
 
-const Searchbar = () => {
-  return (
-    <header class="searchbar">
-    <form class="form">
-      <button type="submit" class="button">
-        <span class="button-label">Search</span>
-      </button>
-  
-      <input
-        class="input"
-        type="text"
-        autocomplete="off"
-        autofocus
-        placeholder="Search images and photos"
-      />
-    </form>
-  </header>
-  )
+class Searchbar extends Component {
+  state = {
+    img: '',
+  };
+
+  handleChange = ({ target }) => {
+    const value = target.value;
+
+    this.setState({
+      img: value.toLowerCase(),
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { img } = this.state;
+    const { onSubmit } = this.props;
+
+    if (img.trim() === '') {
+      toast.warning('Tap some word for searching!');
+      return;
+    }
+    onSubmit(img);
+    this.clearForm();
+  };
+
+  clearForm = () => {
+    this.setState({ img: '' });
+  };
+  render() {
+    return (
+      <header className={s.searchbar}>
+        <form className={s.form} onSubmit={this.handleSubmit}>
+          <button type="submit" className={s.button}>
+            <span className={s.label}>Search</span>
+          </button>
+
+          <input
+            value={this.state.img}
+            onChange={this.handleChange}
+            className={s.input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
 
-export default Searchbar
+export default Searchbar;
