d19253304cc4649d97847987061a6cc8651a3c14
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-15 13:46:23 +0200

    added imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import shortid from 'shortid';
 import s from './App.module.css';
+import Searchbar from 'components/Searchbar';
+import ImageGallery from 'components/ImageGallery';
+import Button from 'components/Button';
+import Loader from 'components/Loader';
+import Modal from 'components/Modal';
+import { fetchMovies } from './utilities/apiService';
+
 export const App = () => {
   return <div></div>;
 };
