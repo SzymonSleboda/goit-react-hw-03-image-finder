adff9ea6d943b17e5b5007f305171dc67979d796
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-15 14:15:49 +0200

    added loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import SpinnerLoader from 'react-loader-spinner';
+import s from './Loader.module.css';
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className={s.loader}>
+    <SpinnerLoader
+      type="Bars"
+      color="#ffffff"
+      height={80}
+      width={80}
+    />
+  </div>
   )
 }
 
-export default Loader
+export default Loader;
